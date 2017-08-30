c11ff3676f2a820666289a80daa5ef282c15f20e
I. Dan Calinescu <idancalinescu@gmail.com>
2017-08-30 14:38:23 +0300

    updated resolvers

--- src/resolvers/modules/alias.js
+++ src/resolvers/modules/alias.js
@@ -3,10 +3,10 @@
 let path = require('path')
 
 module.exports = (context) => ({
-  'carmel/product/locale': path.join(context.product.dir, "locales", context.product.info.locales[0]),
-  'carmel/product/props': path.join(context.product.dir, 'chunky.js'),
-  'carmel/product': context.product.dir,
-  'carmel/stack': path.resolve(context.stack.dir),
-  'carmel/layout': path.resolve(context.layout.dir),
-  'carmel': path.resolve(context.engine.dir)
+  'chunky/product/locale': path.join(context.product.dir, "locales", context.product.info.locales[0]),
+  'chunky/product/props': path.join(context.product.dir, 'chunky.js'),
+  'chunky/product': context.product.dir,
+  'chunky/stack': path.resolve(context.stack.dir),
+  'chunky/layout': path.resolve(context.layout.dir),
+  'chunky': path.resolve(context.engine.dir)
 })
